22acb609def36365f8070d57eba6a6f5983fbc8f
justroniss <rm96679@fiap.com.br>
2024-09-25 19:41:04 -0300

    Atualizando script

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,24 +1,26 @@
 CREATE TABLE IF NOT EXISTS titulo (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    nome VARCHAR(255) NOT NULL,
-    diretor VARCHAR(255) NOT NULL,
-    ano_de_lancamento DATE NOT NULL,
-    valor_diaria DECIMAL(10, 2) NOT NULL,
-    tipo VARCHAR(50) NOT NULL,
-    disponivel BOOLEAN NOT NULL,
-    aluguel_id UUID,
-    CONSTRAINT fk_aluguel FOREIGN KEY (aluguel_id) REFERENCES aluguel(id) ON DELETE SET NULL
+  id UUID PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL,
+  diretor VARCHAR(255) NOT NULL,
+  ano_de_lancamento DATE NOT NULL,
+  valor_diaria DECIMAL(10, 2) NOT NULL,
+  tipo VARCHAR(50) NOT NULL,
+  disponivel BOOLEAN NOT NULL,
+  aluguel_id UUID
 );
 
 CREATE TABLE IF NOT EXISTS aluguel (
-    id UUID PRIMARY KEY,
-    nome_cliente varchar(255) NOT NULL,
-    id_titulo UUID NOT NULL,
-    data_locacao DATE NOT NULL,
-    data_devolucao DATE NOT NULL,
-    valor_total DECIMAL(10, 2),
-    CONSTRAINT fk_titulo FOREIGN KEY(id_titulo) REFERENCES titulo(id)
+   id UUID PRIMARY KEY,
+   nome_cliente VARCHAR(255) NOT NULL,
+   id_titulo UUID NOT NULL,
+   data_locacao DATE NOT NULL,
+   data_devolucao DATE NOT NULL,
+   valor_total DECIMAL(10, 2)
 );
 
 
+ALTER TABLE titulo
+    ADD CONSTRAINT fk_aluguel FOREIGN KEY (aluguel_id) REFERENCES aluguel(id) ON DELETE SET NULL;
 
+ALTER TABLE aluguel
+    ADD CONSTRAINT fk_titulo FOREIGN KEY (id_titulo) REFERENCES titulo(id) ON DELETE CASCADE;
